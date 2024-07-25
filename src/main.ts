59f4f3830e2f975b9f046c18bc951990e6c7b43e
jeong <jw.jeong@tilda.co.kr>
2024-07-25 23:31:14 +0900

    swagger & cors 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const origin = ['http://localhost'];
+
+  app.enableCors({
+    origin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Neuhastone')
+    .setDescription('뉴하스톤 MVP')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
