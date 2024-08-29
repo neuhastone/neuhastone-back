80dd33edcbc46150c4c083697e75fd75062a19ce
jeong <jw.jeong@tilda.co.kr>
2024-08-29 23:38:44 +0900

    유저 서비스 수정

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  saltOrRounds: number = 10;
+
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
@@ -15,9 +18,13 @@ export class UserService {
     name: string,
     nickname: string,
   ): Promise<User> {
+    const hashedPassword: string = await bcrypt.hash(
+      password,
+      this.saltOrRounds,
+    );
     const newUser = new User({
       email,
-      password,
+      password: hashedPassword,
       name,
       nickname,
     });
@@ -25,5 +32,17 @@ export class UserService {
     await this.userRepository.insert(newUser);
 
     return newUser;
+  }
+
+  async getUser(email: string): Promise<User> {
+    const existedUser = await this.userRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!existedUser) throw new NotFoundException();
+
+    return existedUser;
   }
 }
