0ac3f9522a3033e9461816c88054a4339f38a7b3
jeong <jw.jeong@tilda.co.kr>
2024-09-21 18:57:08 +0900

    init project module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppConfigService } from './configs/configs.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRootAsync(AppConfigService.getDatabaseConfigs()),
     UserModule,
     AuthModule,
+    ProjectModule,
   ],
   controllers: [AppController],
   providers: [AppService],
